8884671064624ba32930ab4c9a63e0f3ebbdf5be
muhmadshaban <moshaban96.eng@gmail.com>
2024-10-09 13:34:55 +0300

    all

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,17 @@
+export default function cleanSet(set, startString) {
+  let res;
+  if (!startString || typeof startString !== 'string') {
+    return '';
+  }
+  res = Array.from(set).filter((word) => typeof word === 'string' && word.startsWith(startString));
+  res = res.map((word) => word.slice(startString.length));
+  return res.join('-');
+export default function cleanSet(set, startString) {
+  let res;
+  if (!startString || typeof startString !== 'string') {
+    return '';
+  }
+  res = Array.from(set).filter((word) => typeof word === 'string' && word.startsWith(startString));
+  res = res.map((word) => word.slice(startString.length));
+  return res.join('-');
+}
